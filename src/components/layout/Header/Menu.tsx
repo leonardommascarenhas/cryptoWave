a874552bcc0cb867c4859197c3bba54515e9289c
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-03-23 03:50:10 -0300

    animations for menu

--- src/components/layout/Header/Menu.tsx
+++ src/components/layout/Header/Menu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { AiOutlineDown, AiOutlineClose } from "react-icons/ai";
 
 interface Props {
@@ -6,22 +6,32 @@ interface Props {
   setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const Menu = ({ toggleMenu, setIsActive }: Props) => {
+  const [animateClass, setAnimate] = useState<string>("animation-none");
+
+  useEffect(() => {
+    if (toggleMenu) {
+      setAnimate("animate-leftRight");
+    }
+    if (animateClass != "animation-none" && !toggleMenu) {
+      setAnimate("animate-rightLeft");
+    }
+  }, [toggleMenu]);
+
   return (
-    <>
-      {toggleMenu ? (
-        <nav className="fixed top-0 left-0 w-full h-full z-20 bg-blue-500">
-          <ul>
-            <li>TESTEEEEEE</li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-          <button onClick={() => setIsActive(!toggleMenu)}>
-            <AiOutlineClose />
-          </button>
-        </nav>
-      ) : null}
-    </>
+    <nav
+      className={`-translate-x-full fixed top-0 left-0 w-full h-full z-20 bg-blue-500 ${animateClass} fill-mode-forwards`}
+    >
+      <ul>
+        <li>TESTEEEEEE</li>
+        <li>TESTEEEEEE</li>
+        <li>TESTEEEEEE</li>
+        <li>TESTEEEEEE</li>
+        <li>TESTEEEEEE</li>
+      </ul>
+      <button onClick={() => setIsActive(!toggleMenu)}>
+        <AiOutlineClose />
+      </button>
+    </nav>
   );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,20 @@ module.exports = {
       fontFamily:{
         roboto: ['Roboto', 'sans-serif']
       },
+      keyframes:{
+      rightToLeft: {
+          '0%': {transform: 'translateX(0)' },
+          '100%': {transform: 'translateX(-100%)' },
+        },
+        leftToRight: {
+          '0%': {transform: 'translateX(-100%)' },
+          '100%': {transform: 'translateX(0)' },
+        },
+      },
+      animation:{
+        leftRight: 'leftToRight 0.9s ease-in-out',
+        rightLeft: 'rightToLeft 0.9s ease-in-out'
+      }
     },
   },
   plugins: [
