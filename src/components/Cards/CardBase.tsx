2024784ce1ade8f38f504b7caae7d396ee0134bc
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-04-07 07:21:12 -0300

    Advancments in the card

--- src/components/Cards/CardBase.tsx
+++ src/components/Cards/CardBase.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper";
 import { IconType } from "react-icons/lib";
@@ -17,24 +17,25 @@ interface CardMainContent {
 
 interface Props {
   icon: IconType;
+  iconStyle?: string;
   slideContentArray: string[];
   secondArray: CardMainContent[];
 }
 
-const CardBase = ({ icon: Icon, slideContentArray, secondArray }: Props) => {
+const CardBase = ({ icon: Icon, iconStyle, slideContentArray, secondArray }: Props) => {
   return (
     <article className="h-52 w-1/3 flex items-center">
       <Swiper pagination={{ clickable: true }} modules={[Pagination, Autoplay]} autoplay={true}>
         {slideContentArray.map((title) => (
           <SwiperSlide className="trendingSlides">
-            <h2>
-              <span>
+            <div className="flex items-center gap-3">
+              <span className={iconStyle}>
                 <Icon size={24} />
               </span>
-              {title}
-            </h2>
+              <h2>{title}</h2>
+            </div>
             {secondArray.map(({ item: { small, name } }, index) => (
-              <div className="flex items-center">
+              <div className="flex items-center gap-3">
                 <span>{index + 1}</span>
                 <img src={small} alt="" className="w-4 h-4 rounded-full" />
                 <h2>{name}</h2>
@@ -49,20 +50,36 @@ const CardBase = ({ icon: Icon, slideContentArray, secondArray }: Props) => {
 
 const CardsDisplay = () => {
   const arrayOfTitles = ["trending", "gainers", "losers"];
-  const { isLoading, isError, data } = useQuery({
-    queryKey: ["trending"],
-    queryFn: async () => {
-      return axios.get("https://api.coingecko.com/api/v3/search/trending").then((res) => {
-        return res.data.coins.splice(0, 3);
-      });
-    },
+
+  const [trending, teste] = useQueries({
+    queries: [
+      {
+        queryKey: ["trending"],
+        queryFn: async () =>
+          axios
+            .get("https://api.coingecko.com/api/v3/search/trending")
+            .then((res) => res.data.coins.splice(0, 3)),
+        staleTime: 300000,
+        refetchOnWindowFocus: false,
+      },
+
+      {
+        queryKey: ["users"],
+        queryFn: async () => axios.get("https://jsonplaceholder.typicode.com/users").then((res) => res.data),
+      },
+    ],
   });
-  if (isLoading) {
-    return <div></div>;
-  }
+
+  if (trending.isLoading) return "Loading Posts...";
+  if (teste.isLoading) return "Loading Users...";
   return (
     <div className="hidden lg:flex items-center justify-center gap-2 mt-4">
-      <CardBase icon={AiTwotoneFire} slideContentArray={arrayOfTitles} secondArray={data} />
+      <CardBase
+        icon={AiTwotoneFire}
+        iconStyle="text-red-600"
+        slideContentArray={arrayOfTitles}
+        secondArray={trending.data}
+      />
     </div>
   );
 };
