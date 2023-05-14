799cea972de5fd69cc9cbc6a3ce4bd3bb68313c4
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-05-14 12:56:47 -0300

    changes

--- src/components/layout/Body/Tables/Item.tsx
+++ src/components/layout/Body/Tables/Item.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import DisplayPercentage from "../../assets/Reusables/DisplayPercentage";
 import { AppContext, QueryContext } from "../../../../App";
 
@@ -27,18 +27,59 @@ const Item = ({
   volume24h,
   circulatingSupply,
 }: Props) => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
   const { currency, currencySymbol } = useContext(AppContext);
   const { btcToExchange } = useContext(QueryContext);
 
+  useEffect(() => {
+    function handleResize() {
+      setIsSmallScreen(window.innerWidth < 1240);
+    }
+
+    // Set initial value on mount
+    handleResize();
+
+    // Listen for window resize events
+    window.addEventListener("resize", handleResize);
+
+    // Clean up event listener on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   function usdToCurrency(num: number) {
     return (num / btcToExchange.rates.usd.value) * btcToExchange.rates[currency].value;
+  }
+
+  function formatWithMagnitude(num: number): string {
+    const absNum = Math.abs(num);
+    const sign = Math.sign(num) === -1 ? "-" : "";
+
+    if (absNum >= 1000000000) {
+      return sign + (absNum / 1000000000).toFixed(2) + "B";
+    }
+    if (absNum >= 1000000) {
+      return sign + (absNum / 1000000).toFixed(2) + "M";
+    }
+    if (absNum >= 1000) {
+      return sign + (absNum / 1000).toFixed(2) + "K";
+    }
+    return sign + absNum.toString();
+  }
+
+  function formatNumber(num: number) {
+    const convertedNumber = usdToCurrency(num);
+    return isSmallScreen
+      ? formatWithMagnitude(convertedNumber)
+      : convertedNumber > 1
+      ? parseFloat(convertedNumber.toFixed(2)).toLocaleString()
+      : parseFloat(convertedNumber.toFixed(8)).toLocaleString();
   }
 
   function formatPrice(num: number) {
     const convertedNumber = usdToCurrency(num);
     return convertedNumber > 1
       ? parseFloat(convertedNumber.toFixed(2)).toLocaleString()
-      : parseFloat(convertedNumber.toFixed(8)).toLocaleString();
+      : convertedNumber.toFixed(7).toLocaleString();
   }
 
   return (
@@ -58,10 +99,10 @@ const Item = ({
       <td>
         <DisplayPercentage num={weekPercentage} />
       </td>
-      <td>{`${currencySymbol}: ${formatPrice(marketCap)}`}</td>
+      <td>{`${currencySymbol}: ${formatNumber(marketCap)}`}</td>
       <td>
         {currencySymbol}
-        {formatPrice(volume24h)}
+        {formatNumber(volume24h)}
       </td>
       <td>{circulatingSupply ? `${circulatingSupply + " " + coinAcronym.toUpperCase()}` : "sem acesso"}</td>
     </tr>
