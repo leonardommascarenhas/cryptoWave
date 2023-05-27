6f88e830b1bdb29d34b437dc5c724cbb51d7077d
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-05-27 17:51:22 -0300

    teste

--- src/components/Buttons&Selects/CoinContactMe/ContactMeIcon.tsx
+++ src/components/Buttons&Selects/CoinContactMe/ContactMeIcon.tsx
@@ -1,27 +1,33 @@
 import { useState } from "react";
 import NotificationBall from "./NotificationBall";
-
+import ContactMeModal from "../../Modals/ContactMeModal";
 interface Props {
   image1: string;
   image2: string;
 }
 
 const ContactMeIcon = ({ image1, image2 }: Props) => {
-  const [isActive, setIsActive] = useState<boolean>();
+  const [isActive, setIsActive] = useState<boolean>(false);
 
   return (
-    <div className="group w-16 fixed bottom-20 right-4 transition-all duration-200 [transform-style:preserve-3d] hover:[transform:rotateY(180deg)] animate-scale cursor-pointer">
-      <div className="absolute inset-0">
-        <img
-          src={image1}
-          className="group-hover:opacity-0 fill-mode-forwards transition-opacity duration-150 rounded-full"
-        />
+    <>
+      <div
+        className="group w-16 fixed bottom-20 right-4 transition-all duration-200 [transform-style:preserve-3d] hover:[transform:rotateY(180deg)] animate-scale cursor-pointer"
+        onClick={() => setIsActive(true)}
+      >
+        <div className="absolute inset-0">
+          <img
+            src={image1}
+            className="group-hover:opacity-0 fill-mode-forwards transition-opacity duration-150 rounded-full"
+          />
+        </div>
+        <div className="absolute inset-0 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+          <img src={image2} alt="" className="rounded-full " />
+        </div>
+        {!isActive && <NotificationBall />}
       </div>
-      <div className="absolute inset-0 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-        <img src={image2} alt="" className="rounded-full " />
-      </div>
-      <NotificationBall />
-    </div>
+      {isActive && <ContactMeModal />}
+    </>
   );
 };
 
---
+++ src/components/Modals/ContactMeModal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactMeModal = () => {
+  return (
+    <div className="flex justify-center items-center fixed top-0 left-0 right-0 bottom-0">
+      <div className="w-8 h-8 z-50 bg-black"></div>
+    </div>
+  );
+};
+
+export default ContactMeModal;
