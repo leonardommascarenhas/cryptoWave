c7a0c33e1fb267bebf07f56fe3e72b6558fec8a0
Elias <ecotrim@tvmarkets.com>
2023-06-12 00:37:18 -0300

    modal changes

--- src/components/Modals/ContactMeModal.tsx
+++ src/components/Modals/ContactMeModal.tsx
@@ -32,12 +32,24 @@ const ContactMeModal = () => {
             className={`flex flex-col lg:flex-row justify-center items-center gap-3 z-50 w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 animate-modalOpen`}
           >
             <img className="rounded-full w-16" src={Hi} alt="Hi" />
-            <div className="w-full h-[50vh] sm:h-[70vh] md:h-[60vh] bg-slate-500 shadow-lg rounded-xl pt-24">
-              <p className="opacity-0 animate-rightLeft delay-300 fill-mode-forwards">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio excepturi cupiditate dolor
-                laborum libero delectus blanditiis voluptates consequuntur, saepe omnis a. Rerum dolorem
-                eveniet accusantium consectetur maiores omnis amet et.
-              </p>
+            <div className="w-full bg-slate-500 shadow-lg rounded-xl">
+              <div className="opacity-0 animate-rightLeft delay-300 fill-mode-forwards p-4 flex flex-col gap-3">
+                <p className="text-white">Bem-vindo à CryptoWave!</p>
+
+                <p className="text-white">
+                  Esse site foi montado com a API gratuita da CoinGecko. A versão gratuita dessa API só nos
+                  permite realizar 10 queries por minuto.
+                </p>
+
+                <p className="text-white">
+                  Enquanto você navega pelo site, é bom lembrar que algumas coisas podem ficar temporariamente
+                  indisponíveis se a gente passar do limite da API.
+                </p>
+                <p>
+                  Mas pode ficar tranquilo: eu fiz de tudo para garantir que as queries sejam o mais
+                  eficientes possíveis para não afetar a sua experiência.
+                </p>
+              </div>
             </div>
           </div>
         </section>
