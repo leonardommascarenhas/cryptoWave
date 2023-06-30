8a2bd2c292641e5f1a10ec87891f67ae39a33ef2
Elias <ecotrim@tvmarkets.com>
2023-06-30 12:46:46 -0300

    Modal completed

--- src/components/Modals/ContactMeModal.tsx
+++ src/components/Modals/ContactMeModal.tsx
@@ -25,29 +25,24 @@ const ContactMeModal = () => {
   return (
     <>
       {isOpen && (
-        <section className="fixed top-0 left-0 right-0 bottom-0 w-screen h-screen flex justify-center items-center z-40">
-          <div className="bg-black opacity-70 fixed inset-0 z-40" onClick={handleClose}></div>
+        <section className="fixed top-0 left-0 right-0 bottom-0 w-screen h-screen flex justify-center items-center z-40 text-justify font-poppins">
+          <div className="bg-black opacity-80 fixed inset-0 z-40" onClick={handleClose}></div>
           <div
             ref={modalRef}
             className={`flex flex-col lg:flex-row justify-center items-center gap-3 z-50 w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 animate-modalOpen`}
           >
             <img className="rounded-full w-16" src={Hi} alt="Hi" />
-            <div className="w-full bg-slate-500 shadow-lg rounded-xl">
+            <div className="w-full bg-gray-100 dark:bg-gray-200 shadow-lg rounded-xl">
               <div className="opacity-0 animate-rightLeft delay-300 fill-mode-forwards p-4 flex flex-col gap-3">
-                <p className="">Bem-vindo à CryptoWave!</p>
-
-                <p className="">
-                  Esse site foi montado com a API gratuita da CoinGecko. A versão gratuita dessa API só nos
-                  permite realizar 10 queries por minuto.
-                </p>
-
-                <p className="">
-                  Enquanto você navega pelo site, é bom lembrar que algumas coisas podem ficar temporariamente
-                  indisponíveis se a gente passar do limite da API.
+                <h1 className="text-2xl font-bold">Bem-vindo à CryptoWave!</h1>
+                <p>
+                  Esse site foi montado com a API gratuita da CoinGecko. A versão gratuita dessa API só nos permite realizar 10 queries por minuto.
                 </p>
                 <p>
-                  Mas pode ficar tranquilo: eu fiz de tudo para garantir que as queries sejam o mais
-                  eficientes possíveis para não afetar a sua experiência.
+                  Enquanto você navega pelo site, é bom lembrar que algumas coisas podem ficar temporariamente indisponíveis ou sem acesso se a gente passar do limite da API.
+                </p>
+                <p>
+                  Mas pode ficar tranquilo: eu fiz de tudo para garantir que as queries sejam o mais eficientes possíveis para não afetar a sua experiência.
                 </p>
               </div>
             </div>
